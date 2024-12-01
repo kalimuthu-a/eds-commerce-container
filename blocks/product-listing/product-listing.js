ecee93f6692e334fcbd0c643f603f3233fa4719d
Kalimuthu Aruchamy (Digital,CORP)(X-Adobe) <Kalimuthu.x.Aruchamy@GOINDIGO.IN>
2024-12-01 22:53:30 +0530

    product listing mf entry point updated

--- blocks/product-listing/product-listing.js
+++ blocks/product-listing/product-listing.js
@@ -1,19 +1,22 @@
-const loadRemoteModule = async (elem) => {
+const loadRemoteModule = async (elem, config) => {
   try {
     // Initialize the remote module
     const container = window.productListing; // Global name of the remote
     // await container.init(__webpack_share_scopes__.default); // Initialize sharing scope (if using Webpack 5)
     const factory = await container.get('./ProductListingApp'); // Get the module from the remote
     const productListingApp = factory().default;
-    productListingApp(elem);
+    productListingApp(elem, config);
   } catch (err) {
     console.error('Error loading remote module', err);
   }
 };
 
 export default async function decorate(block) {
+  const config = {
+    producListingBffEndpoint: 'https://products-bff.onrender.com/get-products',
+  }
   const appHolder = document.createElement('div');
   appHolder.id = 'product-listing-app';
-  await loadRemoteModule(appHolder);
+  await loadRemoteModule(appHolder, config);
   block.appendChild(appHolder);
 }
